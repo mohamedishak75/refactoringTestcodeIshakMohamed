dc05361d08f2927cec643745cc4a0f88affc7c80
ishak <ishakmohamed13@gmail.com>
2024-12-13 14:52:48 +0100

    add switch case and method

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,43 +17,68 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
 
-      // premiere switch case
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+  private updateQualityAgedBrie(item: Item) {
+    if (item.quality < 50) {
+      item.quality++;
+    }
+  }
+
+  private updateBackstagePass(item: Item) {
+    if (item.quality < 50) {
+      item.quality++;
+      this.incrimentQuality(item,50,11);
+      this.incrimentQuality(item,50,6);
+    }
+  }
+
+  private decrementQualityByDiiferentName(item: Item , itemName: string){
+    if (item.name != itemName) {
+      item.quality --;
+    }
+  }
+  private updateNormalItem(item: Item) {
+    if (item.quality > 0) {
+      item.quality--;
+    }
+  }
+
+
+  private updateItemQuality(item: Item) {
+    switch (item.name) {
+      case 'Aged Brie':
+        this.updateQualityAgedBrie(item);
+        break;
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        this.updateBackstagePass(item);
+        break;
+      case 'Sulfuras, Hand of Ragnaros':
+        //  rien a faire 
+        return;
+      default:
+        this.updateNormalItem(item);
+        break;
+    }
+  }
+
+
+  private incrimentQuality (item: Item , maxQuality : number, maxSellin : number){
+    if (item.sellIn < maxSellin) {
+      if (item.quality < maxQuality) {
+        item.quality = item.quality + 1
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+    }
+  }
+
+  updateQuality() {
+    this.items.map( (item)  =>  {
+    this.updateItemQuality(item);
+    this.decrementQualityByDiiferentName(item,'Sulfuras, Hand of Ragnaros' )
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
+              this.decrementQualityByDiiferentName(item,'Sulfuras, Hand of Ragnaros' );
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality
@@ -64,7 +89,7 @@ export class GildedRose {
           }
         }
       }
-    }
+    });
 
     return this.items;
   }
